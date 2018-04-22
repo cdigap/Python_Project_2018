49351525ce64bb1432077d2862555eec663febc3
cdigap <36169806+cdigap@users.noreply.github.com>
2018-04-22 21:46:22 +0100

    Update Iris_Data_Analysis_Menu.py

--- Iris_Data_Analysis_Menu.py
+++ Iris_Data_Analysis_Menu.py
@@ -10,13 +10,16 @@ from pandas.plotting import scatter_matrix
 
 import matplotlib.pyplot as plt
 
+#Assigning the data File to dfile
 dfile = "iris_data.csv"
-colms = ['Sepal_Length','Sepal_Width','Petal_Length','Petal_Width','Species']
-# data
 
+#Defining Headers to the file
+colms = ['Sepal_Length','Sepal_Width','Petal_Length','Petal_Width','Species']
+
+# data File getting the Headers assigned.
 data_file = pd.read_csv(dfile,names=colms)
 
-
+# Defining a Menu function
 def menu():
     print('1. View Info')
     print('2. View Summary')
@@ -26,26 +29,31 @@ def menu():
     print('6. Plot Scatter Matrix')
     print('7. Exit')
 
+
+    
 while True:
     menu()
-    user_input = int(input("Enter a Number : "))
-    if user_input == 1:
-        print(data_file.info())
-    elif user_input == 2:
-        print(data_file.describe())
-    elif user_input == 3:
-        print(data_file)
-    elif user_input == 4:
-        data_file.plot(kind='box', subplots = True, layout = (1,4), sharex = False, sharey = False)
-        plt.show()
-    elif user_input == 5:
-        data_file.hist(bins=15)
-        plt.show()
-    elif user_input == 6:
-        scatter_matrix(data_file)
-        plt.show()
-    elif user_input == 7:
-        print('Thanks a lot ....')
-        break
-    else :
-        print('Invalid Option Selected... Please Select from 1 to 7...')
+    try:
+        user_input = int(input("Enter a Number : "))
+        if user_input == 1:
+            print(data_file.info())
+        elif user_input == 2:
+            print(data_file.describe())
+        elif user_input == 3:
+            print(data_file)
+        elif user_input == 4:
+            data_file.plot(kind='box', subplots = True, layout = (1,4), sharex = False, sharey = False)
+            plt.show()
+        elif user_input == 5:
+            data_file.hist(bins=15)
+            plt.show()
+        elif user_input == 6:
+            scatter_matrix(data_file)
+            plt.show()
+        elif user_input == 7:
+            print('Thank you for using the Project - I hope this was helpfull....')
+            break
+        else :
+            print('Invalid Option Selected... Please Select from 1 to 8...')
+    except:
+        print('Exception...Invalid Selection... Please Select from 1 to 8...')
